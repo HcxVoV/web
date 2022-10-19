74046f4010cb86a97ca1b64f1786b2eca29cec59
HcxVoV <1529047183@qq.com>
2022-10-19 19:53:49 +0800

    项目架构

---
+++ src/layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div>
+    <Header></Header>
+    <Content></Content>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import Header from "./header/index.vue"
+import Content from "./content/index.vue"
+
+import { ref, reactive } from 'vue'
+
+</script>
+
+<style scoped lang="less">
+
+</style>
